68d8417ef57e5ffb0277d96fa829f1e0a9843530
Ralph Almarez <ralph.almarez@octaltech.net>
2019-11-30 06:58:41 +0800

    Updates

---
+++ precache-manifest.86e128b90be2d714a9fb052eda368120.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "450a5af1acc2c74e4306ff0827cfc17b",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "f843269cccf4cc30af3a",
+    "url": "/monster-rolodex/static/css/main.3341f798.chunk.css"
+  },
+  {
+    "revision": "8256d7d0304953484fc5",
+    "url": "/monster-rolodex/static/js/2.f5605220.chunk.js"
+  },
+  {
+    "revision": "f843269cccf4cc30af3a",
+    "url": "/monster-rolodex/static/js/main.e3078103.chunk.js"
+  },
+  {
+    "revision": "6f70f97b8cfdb2d06998",
+    "url": "/monster-rolodex/static/js/runtime-main.aec4d7d7.js"
+  }
+]);
